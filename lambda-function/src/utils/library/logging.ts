957eb3b92aef9ea4f0870b489bdc64dd992235de
Greg Akins <gregory.akins@icf.com>
2024-01-22 14:20:42 -0500

    MAT-6584: Fixing issue from PR review

--- lambda-function/src/utils/library/logging.ts
+++ lambda-function/src/utils/library/logging.ts
@@ -1,7 +1,6 @@
 import chalk from "chalk";
 
 export default class Logging {
-  // public static log = (args:any)=> this.info(args);
   public static log = (args: any) => this.info(args);
   public static info = (args: any) =>
     console.log(
